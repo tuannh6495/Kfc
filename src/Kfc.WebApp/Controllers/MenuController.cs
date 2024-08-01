761c92785229c64dde1bac3bc450de277283d535
ducquy100403 <ngducquy104203@gmail.com>
2024-08-01 17:41:59 +0700

    đã thêm các controller và view tương ứng

---
+++ src/Kfc.WebApp/Controllers/MenuController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Kfc.WebApp.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Promotions()
+        {
+            return View();
+        }
+
+        public IActionResult NewItems()
+        {
+            return View();
+        }
+
+        public IActionResult SingleCombo()
+        {
+            return View();
+        }
+
+        public IActionResult GroupCombo()
+        {
+            return View();
+        }
+
+        public IActionResult Chicken()
+        {
+            return View();
+        }
+
+        public IActionResult MainCourse()
+        {
+            return View();
+        }
+
+        public IActionResult Snacks()
+        {
+            return View();
+        }
+
+        public IActionResult Beverages()
+        {
+            return View();
+        }
+    }
+}
